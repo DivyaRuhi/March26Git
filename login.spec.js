1c26997b40eb174bf105086a8c2abed62764fd2b
DivyaRuhi <divyaravichandran2026@gmail.com>
2026-03-28 12:20:11 +0530

    delete

--- login.spec.js
+++
@@ -1,6 +0,0 @@
-
- import { test } from "@playwright/test";
-test("Navigate to URL of Insta", async({page})=>{
-    await page.goto("https://www.instagram.com/");
-    await page.waitForTimeout(5000);
-})
